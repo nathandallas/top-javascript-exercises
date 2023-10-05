8a2f46d82df258d8a56f356b927752944fdee089
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-10-04 23:36:44 -0400

    finished 9-10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let string = input.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let reversed = string.split("").reverse().join("");
+  if (string === reversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    if (n === 0) return 0;
 
+    let firstLast = 1
+    let secondLast = 0
+
+
+    for (let i = 2; i <= n; i++) {
+        let current = firstLast + secondLast;
+
+        secondLast = firstLast;
+        firstLast = current;
+
+    }
+    return firstLast;
 };
 
 // Do not edit below this line
